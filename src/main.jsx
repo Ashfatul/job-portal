0831a865c5ef85923fe6b480bc2c62cb8c428315
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-04 19:34:31 +0600

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from './components/Home';
-import Statistics from './components/Statistics';
-import AppliedJobs from './components/AppliedJobs';
-import Blog from './components/Blog';
-import JobDetails from './components/JobDetails';
-import AllJobs from './components/AllJobs';
-import Error404 from './components/Error404';
+import Home from "./components/Home";
+import AppliedJobs from "./components/AppliedJobs";
+import Blog from "./components/Blog";
+import JobDetails from "./components/JobDetails";
+import AllJobs from "./components/AllJobs";
+import Error404 from "./components/Error404";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: "statistics",
-        element: <Statistics/>,
-      },
-      {
-        path: "appliedJobs",
-        element: <AppliedJobs/>,
-      },
-      {
-        path: "blog",
-        element: <Blog/>,
-      },
-      {
-        path: "/job/:jobID",
-        element: <JobDetails/>,
-      },
-      {
-        path: "/all-jobs",
-        element: <AllJobs/>,
-      },
-      {
-        path: "*",
-        element: <Error404/>,
-      }
-    ]
-  },
+   {
+      path: "/",
+      element: <App />,
+      children: [
+         {
+            path: "/",
+            element: <Home />,
+         },
+         {
+            path: "appliedJobs",
+            element: <AppliedJobs />,
+         },
+         {
+            path: "blog",
+            element: <Blog />,
+         },
+         {
+            path: "/job/:jobID",
+            element: <JobDetails />,
+         },
+         {
+            path: "/all-jobs",
+            element: <AllJobs />,
+         },
+         {
+            path: "*",
+            element: <Error404 />,
+         },
+      ],
+   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+   <RouterProvider router={router} />
 );
